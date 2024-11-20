5d9af706a995b9ab074518b390c7f413ddd50f00
perci2an <psmim1216@naver.com>
2024-11-20 16:18:30 +0900

    #3.9 Group Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-yellow-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-sm flex flex-col gap-4">
-        {["Cian", "Rion", "Joshua", "Kevin "].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl"
-          >
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-sm flex flex-col gap-3">
+        {["Cian", "Rion", "Joshua", "Kevin", ""].map((person, index) => (
+          <div key={index} className="flex items-center group gap-5">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium">{person}</span>
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
+              {person}
+            </span>
             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>{index}</span>
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
             </div>
           </div>
         ))}

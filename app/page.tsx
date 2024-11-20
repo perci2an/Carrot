2e59d993d024de961d782f084be30544c5e08d85
perci2an <psmim1216@naver.com>
2024-11-20 15:55:43 +0900

    #3.8 Map application using Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-yellow-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow *:md:text-red-100 has-[:invalid]:bg-red-100 has-[:invalid]:ring">
-        <input
-          className="w-full rounded-full h-10 py-3 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className="bg-black bg-opacity-70 text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium md:px-10 bg-black">
-          Login in
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-sm flex flex-col gap-4">
+        {["Cian", "Rion", "Joshua", "Kevin "].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl"
+          >
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );

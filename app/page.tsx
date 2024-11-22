fcc81123a97abcf9b3cd574298408f1a5f02f655
perci2an <psmim1216@naver.com>
2024-11-22 13:54:32 +0900

    apply를 이용한 속성 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-yellow-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+    <main className="main-class">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-sm flex flex-col gap-3">
         <div className="group flex flex-col">
           <input
